3f0fa445ad441a252e93a3b779cd65b9e066d07d
hideki-okawa <h.okawa0303@gmail.com>
2023-08-08 08:52:22 +0900

    sampleのmigrate

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: samples
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Sample < ApplicationRecord
 end
