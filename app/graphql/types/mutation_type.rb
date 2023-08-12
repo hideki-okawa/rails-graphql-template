7a512675d25e9165763141dfa984131010576ac0
hideki-okawa <h.okawa0303@gmail.com>
2023-08-12 19:24:25 +0900

    サンプル削除のmutationを実装

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,6 +6,9 @@ module Types
     field :create_sample,
           mutation: Mutations::CreateSample,
           description: 'サンプルを作成する'
+    field :delete_sample,
+          mutation: Mutations::DeleteSample,
+          description: 'サンプルを削除する'
     field :update_sample,
           mutation: Mutations::UpdateSample,
           description: 'サンプルを更新する'
