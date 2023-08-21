f18f6bdc282cf982e2fc4198b82748ab569ed573
hideki-okawa <h.okawa0303@gmail.com>
2023-08-22 08:41:50 +0900

    Bulletの導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,16 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Bullet
+  if Bullet.enable?
+    config.before(:each) { Bullet.start_request }
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
